e760fc1693cd443c3289805f17db062d5380f78a
Gaby4545 <g845845@gmail.com>
2024-05-22 19:47:20 -0400

    Made simple downloadFile function

--- download.lua
+++ download.lua
@@ -1,8 +1,14 @@
---url = "https://example.tweaked.cc"
-local url = "https://raw.githubusercontent.com/Gaby4545/ComputerCraftRemoteDL/main/launch.lua"
-local res = http.get(url)
+local baseUrl = "https://raw.githubusercontent.com/Gaby4545/ComputerCraftRemoteDL/main/"
 
+function downloadFile(baseUrl, filePath)
+    local url = baseUrl + filePath
+    local res = http.get(url)
 
-local file = fs.open("launch.lua", "w")
-file.write(request)
-file.close()
+    local file = fs.open(filePath, "w")
+    file.write(request)
+    file.close()
+end
+
+downloadFile(baseUrl, "download.lua")
+
+shell.run("launch")
