8a4a23d72e3110c4a786aafa184f3dc113302b6b
Gaby4545 <g845845@gmail.com>
2024-05-22 20:46:54 -0400

    fixed parallel

--- launch.lua
+++ launch.lua
@@ -2,26 +2,24 @@ print("Hello Rolz!")
 
 local chatBox = peripheral.find("chatBox")
 
-function update() shell.run("download") end
+local function update() shell.run("download") end
 
-function waitForChat()
+local function waitForChat()
     local event, username, message, uuid, isHidden = os.pullEvent("chat")
     processChat(event, username, message, uuid, isHidden)
 end
 
-function processChat(event, username, message, uuid, isHidden)
+local function processChat(event, username, message, uuid, isHidden)
     if message == "$update" or message == "$u" then
         update() 
     end
 end
 
-function waitForKey()
+local function waitForKey()
     local event, key, isHeld = os.pullEvent("key")
     if key == keys.up then
         update()
     end
 end
 
-while true
-    parallel.waitForAny(waitForChat, waitForKey)
-end 
+parallel.waitForAny(waitForChat, waitForKey)
