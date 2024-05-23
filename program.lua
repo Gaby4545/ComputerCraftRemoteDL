841ae380f04884f74df1f322faa6d9a279dfab37
Gaby4545 <g845845@gmail.com>
2024-05-22 23:27:40 -0400

    Adempt to fix flashing in AR goggles

--- program.lua
+++ program.lua
@@ -7,11 +7,14 @@ else
 end
 
 function mainLoop()
+    ar.clear()
+    ar.drawItemIcon("minecraft:dirt", 10, 20)
+    ar.drawString("dirt", 30, 25, 0xffffff)
+
     while true do
         if ar ~= nil then
-            ar.clear() 
-            ar.drawString(os.date(), 10, 10, 0xffffff)
-            ar.drawString("dirt", 30, 25, 0xffffff)
+            ar.clearElement("time") 
+            ar.drawStringWithId("time", os.date(), 10, 10, 0xffffff)
         end
 
         sleep(1);
