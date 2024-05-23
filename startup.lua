4845be5c4e18ad71be37b04623385cd8f0258143
Gaby4545 <g845845@gmail.com>
2024-05-23 11:43:46 -0400

    Removed monitor global variable

--- startup.lua
+++ startup.lua
@@ -5,26 +5,22 @@ faces = peripheral.getNames()
 
 -- This is the terminal on the computer itself
 terminal = term.native()
- 
+
 -- Global table that will contain all peripherals
 peripherals = {} -- Stores all connected peripherals
- 
+
 print("Finding Peripherals")
 -- Iterate over all peripherals and store them in the table
 for i, face in ipairs(faces) do
     type = peripheral.getType(face)
- 
+
     print("Found ".. type .."@".. face) 
- 
+
     -- Create the table if it does not exist
     if peripherals[type] == nil then
         peripherals[type] = {}
     end
- 
+
      -- Storing the peripheral
     table.insert(peripherals[type], peripheral.wrap(face))
 end
- 
--- Stores the main monitor
-monitor = peripherals.monitor[1]
-
