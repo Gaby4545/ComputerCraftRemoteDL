24e99b809c6ada4d50570df9ba99d93ea4caeea2
Gaby4545 <g845845@gmail.com>
2024-05-28 11:14:45 -0400

    added setCursor

--- apps/tactileGUI.lua
+++ apps/tactileGUI.lua
@@ -2,15 +2,21 @@ terminal = term.current()
 
 window = nil
 
+
+buttons = nil
+
 function waitForClick()
     while true do
         local event, button, x, y = os.pullEvent("mouse_click")
+        window.clear()
+        window.setCursorPos(0, 0)
         window.write("Button" .. button .. "was clicked at" .. x .. "," .. y)
     end
 end
 
 --- The start of it all
 function main() 
+    terminal.clear()
     window = window.create(terminal, 1, 1, 20, 5)
     window.setBackgroundColour(colours.grey)
     window.setTextColour(colours.white)
