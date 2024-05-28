8c02b2cdd510d6addf9becf6fef3b7ba866c861f
Gaby4545 <g845845@gmail.com>
2024-05-28 12:45:00 -0400

    Start of actual GUI

--- apps/tactileGUI.lua
+++ apps/tactileGUI.lua
@@ -1,9 +1,15 @@
 terminal = term.current()
+termX, termY = terminal.getSize()
+
 
 window = nil
+actionBar = nil
 
+buttons = {}
 
-buttons = nil
+function handleClick() 
+
+end
 
 function waitForClick()
     while true do
@@ -11,17 +17,25 @@ function waitForClick()
         window.clear()
         window.setCursorPos(1, 1)
         window.write("Btn" .. button .. ": " .. x .. "," .. y)
+
+        handleClick(button, x, y) 
     end
 end
 
 --- The start of it all
 function main() 
     terminal.clear()
-    window = window.create(terminal, 1, 1, 20, 5)
-    window.setBackgroundColour(colours.grey)
+    window = window.create(terminal, 1, 1, termX, termY-1)
+    window.setBackgroundColour(0x020202)
     window.setTextColour(colours.white)
     window.clear()
     window.write("Testing my window!")
+
+    actionBar = window.create(terminal, 1, termY-1, termX, termY-1)
+
+    actionBar.setCursorPos(1, 2)
+    actionBar.setBackgroundColour(colours.grey)
+    actionBar.write("Back")
 
     waitForClick()
 end
